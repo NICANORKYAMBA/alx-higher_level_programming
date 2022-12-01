4832029cf47f9cb378891046ba694780e598649e
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-12-01 15:00:49 +0300

    editing command line arguments file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     n = len(sys.argv)
     length = (len(sys.argv) - 1)
     if length == 0:
-        print(f"{length} arguments")
+        print(f"{length} arguments.")
     elif length == 1:
-        print(f"{length} argument")
+        print(f"{length} argument:")
         for i in range(1, n):
             print(f"{i:d}: {sys.argv[i]}")
     else:
-        print(f"{length} arguments")
+        print(f"{length} arguments:")
         for i in range(1, n):
             print(f"{i:d}: {sys.argv[i]}")
