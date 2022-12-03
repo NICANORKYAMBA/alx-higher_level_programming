c9613e5ab9e5bd8f38c59bad74a28787ddd95ee9
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-12-03 09:18:17 +0300

    task 1 requirements

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    n = len(my_list) - 1
+    n = len(my_list)
 
-    if idx < 0:
-        return None
-    elif idx > n:
-        return None
-    else:
-        my_list[idx] = element
-        return my_list
+    for number in my_list:
+        if idx < 0:
+            return my_list
+        elif idx > n:
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
