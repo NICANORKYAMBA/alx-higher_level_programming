1ee7785ff27eb35f0dedd26fd1e906ac00bb731c
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-12-03 08:47:50 +0300

    requirement check for task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,12 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
 
     n = len(my_list)
 
-    if idx >= 0 and idx < n:
+    if idx < 0:
+        return my_list
+    elif idx > n:
+        return my_list
+    else:
         del my_list[idx]
-    return my_list
+        return my_list
