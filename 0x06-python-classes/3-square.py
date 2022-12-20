5953c7660e6d99b9a4af753cf0295994abdfec37
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2022-12-20 13:09:43 +0300

    pycodestyle and doctring fixation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,17 @@
-class Square:
+#!/usr/bin/python3
+"""defines a class square
 
+    Raises:
+        TypeError: size should be an integer
+        ValueError: value of size always 0 or greater than 0
+
+    Returns:
+        Area: area of square for both original and initial size
+    """
+
+
+class Square:
+    """represent a class square"""
     def __init__(self, size=0):
         self.__size = size
 
@@ -9,4 +21,5 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """determines and returns the area"""
         return (self.__size * self.__size)
