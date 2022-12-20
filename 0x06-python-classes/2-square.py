a2936d4f5d7409a29fb409b48e328dd854cd23f2
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2022-12-20 13:22:32 +0300

    pycodestyle fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,6 +15,5 @@ class Square:
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
