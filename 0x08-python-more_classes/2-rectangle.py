18e314ea67e0d355df222b2383b3c322886eb7fc
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-01-03 14:08:00 +0300

    area and perimeter of the triangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -62,4 +62,4 @@ class Rectangle:
         """return the perimeter of the rectangle"""
         if self.__height == 0 and self.__width == 0:
             return 0
-        return 2 * (self.__height + self.__width)
+        return  ((self.__height * 2) + (self.__width * 2))
