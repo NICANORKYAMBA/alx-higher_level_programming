ad86c9597a8188982ee8feb307b689345ab6071b
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-01-03 14:33:32 +0300

    area and perimeter of the triangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -25,34 +25,30 @@ class Rectangle:
         self.width = width
 
     @property
-    def height(self):
-        """returns height value"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """sets the new value of height"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
-    @property
     def width(self):
-        """returns the value of width"""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets the new value of width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """returns area of the rectangle"""
