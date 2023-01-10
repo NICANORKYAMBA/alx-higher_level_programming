a465dcd90acc895159e41b767456803bd39d65f0
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-01-10 13:40:37 +0300

    JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""module that returns the JSON representation
+of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """returns JSON rep of my_obj"""
+    return (json.dumps(my_obj))
