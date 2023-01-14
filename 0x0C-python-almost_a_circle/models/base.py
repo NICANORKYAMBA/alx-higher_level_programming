f500ad0c5b00dbae5f84a725b6383c640d1fd8d1
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-01-14 13:13:40 +0300

    JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+import json
 
 
 class Base:
@@ -24,3 +25,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns a json string representation of list of dictionaries"""
+        return json.dumps(list_dictionaries)
