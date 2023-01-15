5a238fa4df3b74e600bc661a0682729828b2848a
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-01-15 23:02:05 +0300

    pycodestyle fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -135,4 +135,3 @@ class Base:
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
-        
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,8 +93,8 @@ class Rectangle(Base):
     def __str__(self):
         """Return the print() and str() representation of the Rectangle"""
         return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x,
-                                                 self.y, self.width,
-                                                 self.height)
+                                                       self.y, self.width,
+                                                       self.height)
 
     def to_dictionary(self):
         """returns a dictionary representation of the Rectangle"""
