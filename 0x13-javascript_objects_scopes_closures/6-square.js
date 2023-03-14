0f84407debefc583e0a0e5312159f6333dfb216f
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-14 19:25:09 +0300

    Task 6 corrections

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,7 @@
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('X');
-        }
-        console.log();
-      }
+      this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
