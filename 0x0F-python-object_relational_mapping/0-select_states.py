99d4a459611976a7c6e8b6b2c3935595e9f9ed1b
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-17 13:11:39 +0300

    getting all states in a database table

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    # Get the mysql username, password and database name
-    # from command line arguments
+    """
+    Get the mysql username, password and database name
+    from command line arguments
+    """
     mysql_user = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Connect to the MySQL server
+    """
+    Connect to the MySQL server
+    """
     db = MySQLdb.connect(
             user=mysql_user,
             passwd=mysql_password, db=db_name)
 
-    # Create a cursor object to execute queries
+    """
+    Create a cursor object to execute queries
+    """
     cursor = db.cursor()
 
-    # Execute a SELECT query to get all the states from the database
+    """
+    Execute a SELECT query to get all the states from the database
+    """
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows from the result set
+    """
+    Fetch all the rows from the result set
+    """
     rows = cursor.fetchall()
 
-    # Display the results
+    """
+    Display the results
+    """
     for row in rows:
         print(row)
