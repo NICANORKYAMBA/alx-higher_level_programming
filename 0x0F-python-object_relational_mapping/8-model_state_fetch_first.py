866e8b6c0cd2dcec428db5bf19447c8b9a9b3e0e
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-19 22:12:00 +0300

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
     """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         mysql_username, mysql_password, mysql_db_name), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -33,4 +32,7 @@ if __name__ == "__main__":
     """
     Print the result or "Nothing" if the table is empty
     """
-    print("{}: {}".format(first_state.id, first_state.name))
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
