cb34722c5b9b8a0bdb981772fa3bef4a36fb131b
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-31 12:56:00 +0300

    getting the response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,8 +7,10 @@ found in the header of the response
 import urllib.request
 import sys
 
-url = sys.argv[1]
 if __name__ == "__main__":
+
+    url = sys.argv[1]
+
     with urllib.request.urlopen(url) as response:
         headers = response.headers
 
