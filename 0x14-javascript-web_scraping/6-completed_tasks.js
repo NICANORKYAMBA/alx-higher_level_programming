5289ed4283952b6a7343eed08d6d26b899751311
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-25 16:08:47 +0300

    Fix errors

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,16 @@
 const request = require('request');
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-// Make the API request and get the response
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  // Load the response data into a list of objects
   const tasks = JSON.parse(body);
 
-  // Create a dictionary to hold the number of completed tasks for each user
   const completedTasksByUser = {};
 
-  // Loop through the tasks and count the completed tasks for each user
   tasks.forEach(task => {
     if (task.completed) {
       const userId = task.userId;
@@ -28,6 +24,5 @@ request(apiUrl, (error, response, body) => {
     }
   });
 
-  // Print out the completed tasks for each user in the desired format
-  console.log(completedTasksByUser);
+  console.log(JSON.stringify(completedTasksByUser, null, 2));
 });
